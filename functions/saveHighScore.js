1fecfef4738eb5339fafe694357454bd2d86ebd5
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2021-07-02 19:50:10 +0300

    feat: fix score table

--- functions/saveHighScore.js
+++ functions/saveHighScore.js
@@ -1,4 +1,10 @@
-const { table, getHighScores } = require('./utils/airtable');
+require('dotenv').config();
+const Airtable = require('airtable');
+Airtable.configure({
+    apiKey: process.env.AIRTABLE_API_KEY,
+});
+const base = Airtable.base(process.env.AIRTABLE_BASE);
+const table = base.table(process.env.AIRTABLE_TABLE);
 
 exports.handler = async (event) => {
     if (event.httpMethod !== 'POST') {
@@ -7,19 +13,26 @@ exports.handler = async (event) => {
             body: JSON.stringify({ err: 'That method is not allowed' }),
         };
     }
-
     const { score, name } = JSON.parse(event.body);
     if (typeof score === 'undefined' || !name) {
         return {
-            statusCode: 400,
+            statusCode: 405,
             body: JSON.stringify({ err: 'Bad request' }),
         };
     }
 
     try {
-        const records = await getHighScores(false);
+        const records = await table
+            .select({
+                sort: [{ field: 'score', direction: 'desc' }],
+            })
+            .firstPage();
+        const formattedRecords = records.map((record) => ({
+            id: record.id,
+            fields: record.fields,
+        }));
 
-        const lowestRecord = records[9];
+        const lowestRecord = formattedRecords[9];
         if (
             typeof lowestRecord.fields.score === 'undefined' ||
             score > lowestRecord.fields.score
